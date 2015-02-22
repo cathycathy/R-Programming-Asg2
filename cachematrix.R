781cf69881cd8c185e25a99049f400375467d518
cathycathy <cheungpokwing@gmail.com>
2015-02-22 21:46:20 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,15 @@
 ## Creates a special matrix object with functions to Cache Matrix & Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-	mi <- NULL								# Initialize Null value for Inverse
+	mi <- NULL						# Initialize Null value for Inverse
 	setm <- function(y){					# f(x) for new/changed matrix:
-		x <<- y									# Pass Matrix Value to Cache
-		mi <<- NULL								# & Set Null Value for Inverse
+		x <<- y							# Pass Matrix Value to Cache
+		mi <<- NULL						# & Set Null Value for Inverse
 	}
 	getm <- function() x					# f(x): Pass Matrix value to cache
-	setmi <- function(sol) mi <<- sol		# f(x): Pass Inverse value to Cache
+	setmi <- function(sol) mi <<- sol			# f(x): Pass Inverse value to Cache
 	getmi <- function() mi					# f(x): Get Inverse Value from Cache
-	list(setm=setm, getm=getm,					# House the functions in a list
+	list(setm=setm, getm=getm,				# House the functions in a list
 		 setmi=setmi,
 		 getmi=getmi)
 }
@@ -22,8 +22,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 	mi <- x$getmi()						# Get Inverse Value from Cache
-	if(!is.null(mi)) return(mi)			# <if cache available> Output [End]
-	mi <- solve(x$getm(), ...)			# <if not> Compute Inverse of Input
-	x$setmi(mi)								# Pass result to cache	
-	mi										# Output (Inverse Value)
+	if(!is.null(mi)) return(mi)				# <if cache available> Output [End]
+	mi <- solve(x$getm(), ...)				# <if not> Compute Inverse of Input
+	x$setmi(mi)							# Pass result to cache	
+	mi								# Output (Inverse Value)
 }
